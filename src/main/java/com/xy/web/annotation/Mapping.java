672435602075638e47f08c9293251db2684993c6
yangnan <yekjh3@163.com>
2023-02-08 09:22:29 +0800

    增强一下Mapping添加在类上的支持

--- src/main/java/com/xy/web/annotation/Mapping.java
+++ src/main/java/com/xy/web/annotation/Mapping.java
@@ -4,7 +4,7 @@ import com.xy.web.MsgType;
 
 import java.lang.annotation.*;
 
-@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Mapping {
